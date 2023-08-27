65406fe85eac1de98596347daaa740c611216d8e
khadijaltaf <khadijaltaf008@gmail.com>
2023-08-28 03:02:16 +0500

    last

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,6 +1,138 @@
 import { Router } from "express";
+import multer, { diskStorage } from "multer";
+import jwt from 'jsonwebtoken';
+import { userModel } from "../models/user.js";
+import { blogModel } from "../models/blogs.js";
 
 const adminRouter = Router();
+
+const authMiddleware= async (req,res, next) =>{
+
+    const token = req.headers.token;
+
+    if(!token) {
+        res.status(401).json({message: "Unauthorized"});
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        
+        const user = await userModel.findById({_id:decoded.userId });
+
+        if(!user) {
+            res.status(401).json({message: "Unauthorized"});
+            res.send();
+        }
+
+        next();
+    }
+    catch(err) {
+        res.status(401).json({message: err});
+        res.send();
+    }
+};
+
+
+adminRouter.use(authMiddleware);
+
+adminRouter.post('/createblog' , async(req, res)=>{
+    try {
+        const {title, body, userId} = req.body;
+        const newBlog = await blogModel.create({
+            title,
+            body,
+            user: userId
+        });
+        res.status(201).json({newBlog})
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+adminRouter.post('/updateblog' , async(req, res)=>{
+    try {
+        const {title, body, userId} = req.body;
+        const newBlog = await blogModel.updateOne({
+            title,
+            body,
+            user: userId
+        });
+        res.status(201).json({newBlog})
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+adminRouter.post('/deleteblog' , async(req, res)=>{
+    try {
+        const {title, body, userId} = req.body;
+        const newBlog = await blogModel.findByIdAndRemove({
+            title,
+            body,
+            user: userId
+        });
+        res.status(201).json({newBlog})
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+
+
+// const storage = diskStorage({
+//     destination: (req, file, cb)=>{
+//         cb(null, process.env.UPLOAD_PATH)
+//     },
+//     filename: (req, file, cb) =>{ 
+        
+//         const filename = file.originalname.toLowerCase().split(' ').join('.')
+//         cb(null, filename);
+//     }
+// });
+
+// let upload=  multer({
+//     storage: storage,
+//     fileFilter: (req, file, cb)=>{
+//         if(file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg"){
+//             cb(null, true)
+//         }
+//         else {
+//             cb(null, false)
+//             return cb(new Error("only .jpg,.jpeg and .png extensions allowed"));
+//         }
+//     }
+// })
+
+
+// adminRouter.post('/createblog' ,upload.single('cover') , async (req,res)=>{
+//     try {
+//         console.log(req.body);
+//         const {title, body, userId} = req.body;
+
+//         let uploadurl =  req.protocol + "://" +req.get("host") +"/" + process.env.UPLOAD_PATH +"/" + req.file.filename;
+
+//         // http://localhost:8080/uploads/abc.jpg
+
+//         const newBlog = await blogModel.create({
+//             title, 
+//             body,
+//             user: userId,
+//             cover: uploadurl
+//         });
+
+//         res.status(201).json({newPost});
+//     }
+//     catch(err) {
+//         console.log(err);
+//     }
+// });
+
+
 
 
 export default adminRouter;
--- routes/main.routes.js
+++ routes/main.routes.js
@@ -1,18 +1,110 @@
 import { Router } from "express";
 import { blogModel } from "../models/blogs.js";
+import { userModel } from '../models/user.js';
+import { compare, hash } from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+
 
 const mainRouter = Router();
 //cz we dont need any authentication 
-mainRouter.get('/blogs' , async(req, res)=>{
-    try{
-    const data = await blogModel.find();
-    res.json(data);
-    res.end();
+mainRouter.get('/blogs', async (req, res) => {
+    try {
+
+        const data = await blogModel.find();
+        const count = await blogModel.count();
+
+        if (count > 0) {
+            res.json(data);
+        } else {
+            res.json({ message: "No post found" });
+        }
 
     }
-    catch (error){
+    catch (error) {
         console.log(error);
     }
 });
+
+//single search
+
+mainRouter.get('/blogs/:id', async (req, res) => {
+    try {
+
+        let blogId = req.params.id;
+        const data = await blogModel.findById({ _id: blogId });
+        // const count = await blogModel.count();
+
+        if (data) {
+            res.json(data);
+        } else {
+            res.json({ message: "No post found with specified ID" });
+        }
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+
+//register
+mainRouter.post('/register', async (req, res) => {
+    const { username, email, password } = req.body; //object destructuring
+    const encryptedPassword = await hash(password, 10);
+
+    try {
+        const user = await userModel.create({
+            username,
+            email,
+            password: encryptedPassword
+        });
+
+        res.status(201).json({ message: "User has been registered successfully", user });
+        res.end();
+
+    }
+    catch (err) {
+        console.log(err);
+        res.json({ message: err });
+        res.end();
+    }
+});
+
+
+//login 
+
+mainRouter.post('/login', async (req, res) => {
+
+    const { username, password } = req.body;
+
+
+    try {
+        const user = await userModel.findOne({ username });
+
+        if (!user) {
+            res.status(401).json({ message: "User not found" });
+            res.end();
+        }
+
+        const isPasswordValid = await compare(password, user.password);
+
+        if (!isPasswordValid) {
+            res.status(401).json({ message: "Invalid credentials" });
+            res.end();
+        }
+
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+        res.status(200).json({ token: token });
+        res.end();
+
+    }
+    catch (err) {
+        console.log(err);
+    }
+
+});
+
+
 
 export default mainRouter;
